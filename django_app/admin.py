32b7a485f074e2ddda4862f030cafa04b846eec4
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-09 20:57:13 +0100

    register model and configure display and filter in admin

--- django_app/admin.py
+++ django_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django_app.models import Evento
 
 # Register your models here.
+
+class ShowEventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'usuario', 'data_criacao')
+    list_filter = ('titulo',)
+
+admin.site.register(Evento, ShowEventoAdmin)
