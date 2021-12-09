a64f99fd97b92e823202fe9c462259aefdeebfc5
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-09 20:57:21 +0100

    Evento model

--- django_app/models.py
+++ django_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100, blank=False, null=False)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(null=False)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'evento'
+    
+    def __str__(self):
+        return self.titulo
