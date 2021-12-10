51aa7ae2e1a24317315a019d6f0f4c6b4aded41b
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-10 12:05:10 +0100

    configure index and redirect to not foumd page

--- django_app/views.py
+++ django_app/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import redirect, render, HttpResponse
 from django_app.models import Evento
 
 # Create your views here.
+def index(request):
+    evento = Evento.objects.all()
+
+    # Filter with current user
+    # usuario = request.user
+    # evento_of_user = Evento.objects.filter(user=usuario)
+
+    return render(request, 'pages/home.html', {
+        "evento": evento
+    })
+
 def hello(request, name, age):
     return HttpResponse('<h1>Hello {} de {} anos</h1>'.format(name, age))
 
@@ -10,3 +21,6 @@ def sum_values(request, first: int, last: int) -> HttpResponse:
 
 def evento(request, titulo_evento: str) -> HttpResponse:
     return HttpResponse(f'<h1>{Evento.objects.get(titulo=titulo_evento.capitalize())}</h1>')
+
+# def some_query(request, query: str):
+#     return redirect('/')
