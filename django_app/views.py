5141a8a8660b4dd5cb2e18c53be19fb44965ad3a
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-11 00:01:16 +0100

    show event by id and update

--- django_app/views.py
+++ django_app/views.py
@@ -8,13 +8,22 @@ from django.contrib import messages
 @login_required(login_url="/login/")
 def index(request):
     #evento = Evento.objects.all()
+    id_evento = request.GET.get('id')
+    
+    current_evento = {
+        "evento": {}
+    }
 
+    if id_evento:
+        current_evento["evento"] = Evento.objects.get(id=id_evento)        
+        
     # Filter with current user
     usuario = request.user
     evento_of_user = Evento.objects.filter(usuario=usuario)
 
     return render(request, 'pages/home.html', {
-        "evento": evento_of_user
+        "evento": evento_of_user,
+        "current_evento": current_evento["evento"]
     })
 
 def hello(request, name, age):
@@ -26,23 +35,55 @@ def sum_values(request, first: int, last: int) -> HttpResponse:
 def evento(request, titulo_evento: str) -> HttpResponse:
     return HttpResponse(f'<h1>{Evento.objects.get(titulo=titulo_evento.capitalize())}</h1>')
 
+@login_required(login_url="/login/")
 def create_evento(request):
     if request.POST:
         titulo = request.POST.get('titulo')
         descricao = request.POST.get('descricao')
         data_evento = request.POST.get('data_evento')
         usuario = request.user
+        id_evento = request.POST.get('id')
 
-        if Evento.objects.create(
-                titulo=titulo, 
-                descricao=descricao, 
-                data_evento=data_evento, 
-                usuario=usuario):
-                            
-           messages.error(request, "Evento criado com sucesso")  
+        if id_evento:            
+            Evento.objects.filter(id=id_evento).update(
+                    titulo=titulo, 
+                    descricao=descricao, 
+                    data_evento=data_evento )
+
+            messages.success(request, "Evento actualizado com sucesso") 
+        else:
+            if Evento.objects.create(
+                    titulo=titulo, 
+                    descricao=descricao, 
+                    data_evento=data_evento, 
+                    usuario=usuario):
+                    
+                messages.success(request, "Evento criado com sucesso")          
 
     return redirect('/')
-            
+
+@login_required(login_url="/login/")
+def delete_evento(request, id:int):
+    usuario = request.user
+    evento = Evento.objects.get(id=id)
+    if usuario == evento.usuario:
+        evento.delete()
+
+        messages.success(request, "Evento deletado com sucesso")      
+    
+    return redirect('/')
+
+# @login_required(login_url="/login/")
+# def update_evento(request):
+#     id_evento = request.GET.get('id')
+#     print(id_evento)
+
+#     usuario = request.user
+#     evento_of_user = Evento.objects.filter(usuario=usuario)
+
+#     return render(request, 'pages/home.html', {
+#         "evento": evento_of_user
+#     })
 
 # def some_query(request, query: str):
 #     return redirect('/')
