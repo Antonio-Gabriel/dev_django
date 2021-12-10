b6d236fb2e07769b7d60f799324d68169f2131c9
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-11 00:01:56 +0100

    add id on display_list

--- django_app/admin.py
+++ django_app/admin.py
@@ -4,7 +4,7 @@ from django_app.models import Evento
 # Register your models here.
 
 class ShowEventoAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'data_evento', 'usuario', 'data_criacao')
+    list_display = ('id', 'titulo', 'data_evento', 'usuario', 'data_criacao',)
     list_filter = ('titulo',)
 
 admin.site.register(Evento, ShowEventoAdmin)
